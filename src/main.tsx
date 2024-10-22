695164d17a0d7301ef3c7cb0f7bd6700524dc8d2
mxm <mxm.web.develop@gmail.com>
2024-10-22 11:20:19 +0800

    patch version

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-
 import { createRoot } from 'react-dom/client'
-import Preview from './Preview.tsx'
+import Preview from './Preview' // 修改为不带.tsx后缀
 
 createRoot(document.getElementById('root')!).render(
   <Preview />
